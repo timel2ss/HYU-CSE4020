dec3bf3b8730080cc42e08904e82c45bde019032
이용우 <dyddn0823@hanyang.ac.kr>
2021-04-11 18:58:16 +0900

    Class Assignment 1

--- ClassAssignment1/main.py
+++ ClassAssignment1/main.py
@@ -5,14 +5,17 @@ from OpenGL.GLU import *
 
 x_pos = 0
 y_pos = 0
-x_translate = 0
-y_translate = 0
 
 azimuth = 45
 elevation = 45
 distance = 15
-zoom = 50
-up = 1
+zoom = 30
+
+up = np.array([0., 1., 0.])
+u = np.array([1., 0., 0.])
+v = np.array([0., 1., 0.])
+w = np.array([0., 0., 1.])
+origin = np.array([0., 0., 0.])
 
 leftMouse = False
 rightMouse = False
@@ -21,6 +24,7 @@ rightMouse = False
 toggle = True
 
 def render():
+    global u, v, w
     glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
     glEnable(GL_DEPTH_TEST)
     glPolygonMode( GL_FRONT_AND_BACK, GL_LINE )
@@ -35,16 +39,16 @@ def render():
         # Zooming on Orthogonal projection
         glOrtho(-.14 * zoom, .14 * zoom, -.14 * zoom, .14 * zoom, 5, 1000)
 
-    # Panning
-    glTranslatef(x_translate, 0, 0)
-    glTranslatef(0, y_translate, 0)
-
-    # Orbit
+    # Panning and Orbit
     x_angle = np.radians(azimuth)
     y_angle = np.radians(elevation)
 
-    pos = [distance * np.cos(x_angle) * np.cos(y_angle), distance * np.sin(y_angle), distance * np.sin(x_angle) * np.cos(y_angle)]
-    gluLookAt(pos[0], pos[1], pos[2], 0, 0, 0, 0, up, 0)
+    w = np.array([np.cos(x_angle) * np.cos(y_angle), np.sin(y_angle), np.sin(x_angle) * np.cos(y_angle)])
+    u = np.cross(up, w) / np.sqrt(np.dot(np.cross(up, w), np.cross(up, w)))
+    v = np.cross(u, w) / np.sqrt(np.dot(np.cross(u, w), np.cross(u, w)))
+
+    pos = [distance * w[0] + origin[0], distance * w[1] + origin[1], distance * w[2] + origin[2]]
+    gluLookAt(pos[0], pos[1], pos[2], origin[0], origin[1], origin[2], up[0], up[1], up[2])
 
     drawFrame()
     drawGridOnXZplane()
@@ -52,11 +56,11 @@ def render():
 def drawGridOnXZplane():
     glBegin(GL_LINES)
     glColor3ub(255, 255, 255)
-    for i in range(-5, 6):
-        glVertex3fv(np.array([i, 0, 5]))
-        glVertex3fv(np.array([i, 0, -5]))
-        glVertex3fv(np.array([5, 0, i]))
-        glVertex3fv(np.array([-5, 0, i]))
+    for i in range(-30, 31):
+        glVertex3fv(np.array([i, 0, 30]))
+        glVertex3fv(np.array([i, 0, -30]))
+        glVertex3fv(np.array([30, 0, i]))
+        glVertex3fv(np.array([-30, 0, i]))
     glEnd()
 
 def drawFrame():
@@ -73,21 +77,22 @@ def drawFrame():
     glEnd()
 
 def cursor_position_callback(window, xpos, ypos):
-    global up, azimuth, elevation, x_pos, y_pos, x_translate, y_translate
+    global origin, up, azimuth, elevation, x_pos, y_pos
     if leftMouse == True:
         # Orbit
         azimuth += .4 * (xpos - x_pos)
         elevation += .4 * (ypos - y_pos)
 
         if np.cos(np.radians(elevation)) < 0:
-            up = -1
+            up = np.array([0., -1., 0.])
         else:
-            up = 1
+            up = np.array([0., 1., 0.])
 
     elif rightMouse == True:
         # Panning
-        x_translate += .02 * (xpos - x_pos)
-        y_translate -= .02 * (ypos - y_pos)
+        x_translate = .02 * (xpos - x_pos)
+        y_translate = .02 * (ypos - y_pos)
+        origin -= x_translate * u + y_translate * v
 
     x_pos = xpos
     y_pos = ypos
@@ -105,14 +110,12 @@ def mouse_button_callback(window, button, action, mods):
         elif action == glfw.RELEASE:
             rightMouse = False
 
-
 def scroll_callback(window, xoffset, yoffset):
     global zoom
     zoom -= yoffset
     if zoom < 5:
         zoom = 5
     
-
 def key_callback(window, key, scancode, action, mods):
     global toggle
     if action==glfw.PRESS or action==glfw.REPEAT:
@@ -121,7 +124,6 @@ def key_callback(window, key, scancode, action, mods):
                 toggle = False
             else:
                 toggle = True
-
 
 def main():
     # Initialize the library
